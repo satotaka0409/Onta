3f4c92a24209800b4a80846174488af7d036e925
佐藤孝保 <t1sato@mitani-corp.co.jp>
2026-09-04 16:26:27 +0900

    データ構造と変調方式のドキュメントを更新し、ハッシュ計算ユーティリティを追加

---
+++ Onta_core/src/core/hash.cs
@@ -0,0 +1,33 @@
+using System.Security.Cryptography;
+
+namespace Otofa.Core;
+
+/// <summary>
+/// SHA-256 / SHA-512 ハッシュを計算するユーティリティです。
+/// </summary>
+public static class Hash
+{
+    /// <summary>
+    /// 入力バイト列の SHA-256 ハッシュ値を計算します。
+    /// </summary>
+    /// <param name="data">ハッシュ化対象のバイト列。</param>
+    /// <returns>32 バイトの SHA-256 ハッシュ値。</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="data"/> が null の場合にスローされます。</exception>
+    public static byte[] ComputeSha256(byte[] data)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        return SHA256.HashData(data);
+    }
+
+    /// <summary>
+    /// 入力バイト列の SHA-512 ハッシュ値を計算します。
+    /// </summary>
+    /// <param name="data">ハッシュ化対象のバイト列。</param>
+    /// <returns>64 バイトの SHA-512 ハッシュ値。</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="data"/> が null の場合にスローされます。</exception>
+    public static byte[] ComputeSha512(byte[] data)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        return SHA512.HashData(data);
+    }
+}
